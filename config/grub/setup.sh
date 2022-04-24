70dbe3db8a107346a34253d7bb37db2107744fff
Squashbucklr <2jbwar22@gmail.com>
2022-04-23 23:59:26 -0400

    reset grub to working version

--- config/grub/setup.sh
+++ config/grub/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+CUR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo "[grub] sudo required to write to /etc/default/grub"
 
-cat $HOME/.config/grub/grub | sudo tee /etc/default/grub > /dev/null
+sed s~{CUR}~$CUR~ $CUR/grub.template | sudo tee /etc/default/grub > /dev/null
 
 echo "[grub] make sure to run grub-mkconfig -o /boot/grub/grub.cfg"
